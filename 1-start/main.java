6d0939bcfbfd7d8c08d0d2b60840351eecfe0f81
Ahmad Osman <ahmad@ahmad-osman.com>
2021-01-29 23:48:22 +0100

    first commit

---
+++ 1-start/main.java
@@ -0,0 +1,58 @@
+public class main {
+    // public static void main(String[] args) {
+    //     String data = "Welcome to Java";
+    //     // + for String means compine
+    //     String fName = "Ahmad";
+    //     String lName = "Osman";
+    //     System.out.println(fName + lName);
+    //     System..printl.print("Hello World2");
+    // }
+
+
+    // public static void main(String[] args) {
+    //     float num1 = 0;
+    //     int num2 = 5;
+
+    //     System.out.println(num2  / num1);
+    // }
+//    public static void main(String[] args) {
+
+//         // i++; => i = i + 1; => i += 1;
+//         // for(int i = 1; i <= 10; i += 1) {
+//         //     System.out.println(i);
+//         // }
+
+
+//         // print numbers from 10 to 1
+
+//         for (int i = 10; i >= 1; i--) {
+//             System.out.println(i);
+//         }
+//     }
+
+public static void main(String[] args) {
+
+    for (int i = 1; i <= 10; i++ ){
+        if (i % 2 == 0) {
+            System.out.println(i);
+        }
+        // System.out.println("Hi");
+    }
+}
+
+}
+
+// loop 1
+// i = 1
+// if false
+
+// loop 2
+// i = 2
+// if true
+// print
+
+// loop 3
+// i = 3
+// if false
+
+
